7229f7297a9b2914616e1433783a99607602cc04
lioTauhid <77046554+lioTauhid@users.noreply.github.com>
2022-04-21 22:10:41 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import uuid
+from flask import Flask, jsonify, request, render_template, send_from_directory
+import base64
+import os
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+##################################################################################
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/audio', methods=['POST'])
+def uploadAudioFile():
+    data = request.get_json(force=True)["audioFile"]
+    wav_file = open(f"audio-files/{uuid.uuid4().hex}.wav", "wb")
+    decode_string = base64.b64decode(data)
+    wav_file.write(decode_string)
+    resp = jsonify({'message': 'File successfully uploaded'})
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/audio', methods=['GET'])
+def allAudioFiles():
+    fileNames = os.listdir("audio-files")
+    print(fileNames)
+    resp = jsonify({"messageFilenames": fileNames})
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/play/<path:filename>')
+def play_file(filename):
+    return send_from_directory('audio-files/', filename)
+
+
+####################################################################################
+if __name__ == "__main__":
+    app.run()
